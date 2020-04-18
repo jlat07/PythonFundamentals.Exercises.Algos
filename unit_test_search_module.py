853783b5697b56ea0c233634ab8b63eb2643ecfb
James Thompson <jlat0128@gmail.com>
2020-04-18 18:00:40 -0400

    updating unit test after

--- unit_test_search_module.py
+++ unit_test_search_module.py
@@ -4,31 +4,7 @@ import unittest
 
 class SearchModuleTest(unittest.TestCase):
 
-    def test_binary_search(self):
-        test_cases = [
-            ([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], 6, 6),
-            ([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], 3, 3),
-            ([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 8, 8),
-        ]
-
-        for expected, params in test_cases:
-            with self.subTest(f"{params} -> {expected}"):
-                actual = sm.binary_search(params[0], params[1])
-                self.assertEqual(expected, actual)
-
-    def test_binary_search_random(self):
-            test_cases = [
-                ([i for i in range(1000)], 221, 221),
-                #([i for i in range(1000)], 850, 850),
-                #([i for i in range(10000)], 45, 45),
-                #([i for i in range(10000)], 45, 45)
-            ]
-
-            for expected, params in test_cases:
-                with self.subTest(f"{params} -> {expected}"):
-                    actual = sm.binary_search(params[0], params[1])
-                self.assertEqual(expected, actual)
-            
-            #print
-
-
+    def test_number_value(self):
+        self.assertNotEqual(sm.binary_search([2, 3, 4, 6, 7, 9, 12, 14, 15, 16], 9), 9)
+        self.assertEqual(sm.binary_search([0, 1, 2, 3, 12, 5, 6, 7, 8, 9, 10, 11], 12), 4)
+        self.assertEqual(sm.binary_search([3,6,7,1,5,8,9,11,12,15,13,14,16,17,19,2,4,10,18,20], 5), 3)
