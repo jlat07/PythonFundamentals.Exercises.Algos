e22a0d2bc96bb9f0b8d5c23d48ca85350c23284c
James Thompson <jlat0128@gmail.com>
2020-04-18 12:18:00 -0400

    add seach_module.py

---
+++ search_module.py
@@ -0,0 +1,10 @@
+
+
+def binary_search(list_in: List[int], item: int) -> int:
+    """
+    If item exists in list_in, this function returns its position in the list.
+    """
+    if item in list_in:
+        do something
+    else:
+        print("Item not in list")
