430a5692cfff75cc8290b757ffb88b651ad76dc5
James Thompson <jlat0128@gmail.com>
2020-04-18 15:17:18 -0400

    testing binary search elapsed time verse a standard find in method

--- search_module.py
+++ search_module.py
@@ -1,3 +1,4 @@
+import time
 
 
 def binary_search(list_in: list, item: int) -> int:
@@ -10,17 +11,52 @@ def binary_search(list_in: list, item: int) -> int:
     low = 0
     high = len(list_in) - 1
     found = False
-    while(low <= high and not found):
-        mid = (low + high) // 2
-        if list_in[mid] == item:
-            found = True
-        else:
-            if item < list_in[mid]:
-                high = mid - 1
+    t1 = time.perf_counter()
+
+    try:
+        while(low <= high and not found):
+            mid = (low + high) // 2
+            if list_in[mid] == item:
+                found = True
             else:
-                low = mid + 1
-    print(list_in.index(item))
+                if item < list_in[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+        print(list_in.index(item))
+
+    except ValueError:
+        print(f"{item} is not in list")
+
+    t2 = time.perf_counter()
+    print(f"binary search took {t2-t1} seconds")
 
 
-binary_search([0, 1, 2, 3, 12, 5, 6, 7, 8, 9, 10, 11], 12)
-binary_search([3,6,7,1,5,8,9,11,12,15,13,14,16,17,19,2,4,10,18,20], 5)
+def find(list_in: list, item: int) -> int:
+    t1 = time.perf_counter()
+
+    if item in list_in:
+        print(list_in.index(item))
+    else:
+        print(f"{item} is not in list")
+
+    t2 = time.perf_counter()
+    print(f"find took {t2-t1} seconds")
+
+
+#binary_search([0, 1, 2, 3, 12, 5, 6, 7, 8, 9, 10, 11], 12)
+#binary_search([3,6,7,1,5,8,9,11,12,15,13,14,16,17,19,2,4,10,18,20], 5)
+
+
+test_list = [i for i in range(10000000)]
+n = 1919
+binary_search(test_list, n)
+find(test_list, n)
+
+
+#binary_search([0, 1, 2, 3, 12, 5, 6, 7, 8, 9, 10, 11], 12)
+#binary_search([3,6,7,1,5,8,9,11,12,15,13,14,16,17,19,2,4,10,18,20], 25)
+
+# test_list = [i for i in range(1000)]
+# binary_search(test_list, 999)
+# find(test_list, 999)
--- unit_test_search_module.py
+++ unit_test_search_module.py
@@ -9,13 +9,26 @@ class SearchModuleTest(unittest.TestCase):
             ([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], 6, 6),
             ([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], 3, 3),
             ([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 8, 8),
-            #([i for i in range(n)], (45), 45)
-
         ]
 
         for expected, params in test_cases:
             with self.subTest(f"{params} -> {expected}"):
                 actual = sm.binary_search(params[0], params[1])
                 self.assertEqual(expected, actual)
+
+    def test_binary_search_random(self):
+            test_cases = [
+                ([i for i in range(1000)], 221, 221),
+                #([i for i in range(1000)], 850, 850),
+                #([i for i in range(10000)], 45, 45),
+                #([i for i in range(10000)], 45, 45)
+            ]
+
+            for expected, params in test_cases:
+                with self.subTest(f"{params} -> {expected}"):
+                    actual = sm.binary_search(params[0], params[1])
+                self.assertEqual(expected, actual)
+            
+            #print
 
 
